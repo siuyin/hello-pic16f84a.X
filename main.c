f305454793af76bda9d8655839d76e4a9d07bb64
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-07-09 22:33:01 +0800

    Added wait for tick change in main.

--- main.c
+++ main.c
@@ -19,21 +19,25 @@
 #include <xc.h>
 
 #define _XTAL_FREQ 740000   // 6.8k and 100pF resitor is 10% and capacitor is 20% tolerance
+// I swapped chips and the timing is mostly the same.
+// Accuracy is limited by RC values and not chip manufacturing tolerance.
 
-const unsigned int led_a_flash_period_ms = 5000;
+const unsigned int led_a_flash_period_ticks = 360;
 volatile unsigned int led_a_flash_task_ctr;
 
-const unsigned int led_b_flash_period_ms = 5000;
+const unsigned int led_b_flash_period_ticks = 360;
 volatile unsigned int led_b_flash_task_ctr;
 
-const unsigned char tmr0_1ms_reload_val = 210; // 4T*2=10.88us = tick. 2:prescaler. (256-164)*tick = 1ms Tune accordingly.
+const unsigned char tmr0_reload_val = 248;
+volatile unsigned char tick;
 
 void __interrupt() interrupt_service_routine(void) {
     if (T0IE && T0IF) {
         if (led_a_flash_task_ctr > 0) led_a_flash_task_ctr--;
         if (led_b_flash_task_ctr > 0) led_b_flash_task_ctr--;
+        tick++;
 
-        TMR0 = tmr0_1ms_reload_val;
+        TMR0 = tmr0_reload_val;
         T0IF = 0; // clear TMR0 interrupt flag
         T0IE = 1; // re-enable TMR0 interrupt
     }
@@ -43,14 +47,19 @@ void setup_port_b(void);
 void setup_TMR0_for_interrupts(void);
 void flash_LED_a_task(void);
 void flash_LED_b_task(void);
+void wait_for_next_tick(unsigned char *);
 
 void main(void) {
+    unsigned char current_tick = 0;
+
     setup_port_b();
     setup_TMR0_for_interrupts();
 
     while (1) {
         flash_LED_a_task();
         flash_LED_b_task();
+
+        wait_for_next_tick(&current_tick);
     }
 
     return;
@@ -64,15 +73,21 @@ void setup_port_b(void) {
 }
 
 void setup_TMR0_for_interrupts(void) {
-    led_a_flash_task_ctr = led_a_flash_period_ms;
-    led_b_flash_task_ctr = led_b_flash_period_ms;
+    led_a_flash_task_ctr = led_a_flash_period_ticks;
+    led_b_flash_task_ctr = led_b_flash_period_ticks;
+    tick = 0;
 
+    // example:
+    // Fosc = 1/(2RC)
+    // Tcyc = 4/Fosc
+    // TMR0_t = prescaler * Tcyc
+    // If R=6.8k and C=100pF, and prescaler = 256, each TMR0_t = 1.39 ms
     PSA = 0; // 0: Assign prescaler to TMR0
-    PS2 = 0; // 001: prescaler set to 2
-    PS1 = 0;
+    PS2 = 1; // 001: prescaler set to 2
+    PS1 = 1;
     PS0 = 1;
 
-    TMR0 = tmr0_1ms_reload_val; // setup for 256 - x counts before triggering interrupt.
+    TMR0 = tmr0_reload_val; // setup for 256 - x counts before triggering interrupt.
     T0CS = 0; // 0: TMR0 counter source is internal clock
 
     T0IE = 1; // enable TMR0 interrupt
@@ -83,7 +98,7 @@ void toggle_LED_a(void);
 
 void flash_LED_a_task(void) {
     if (led_a_flash_task_ctr == 0) {
-        led_a_flash_task_ctr = led_a_flash_period_ms;
+        led_a_flash_task_ctr = led_a_flash_period_ticks;
         toggle_LED_a();
     }
 }
@@ -96,7 +111,7 @@ void toggle_LED_b(void);
 
 void flash_LED_b_task(void) {
     if (led_b_flash_task_ctr == 0) {
-        led_b_flash_task_ctr = led_b_flash_period_ms;
+        led_b_flash_task_ctr = led_b_flash_period_ticks;
         toggle_LED_b();
     }
 }
@@ -105,3 +120,8 @@ void toggle_LED_b(void) {
     RB2 = ~RB2;
 }
 
+void wait_for_next_tick(unsigned char * current_tick) {
+    while (*current_tick == tick) {
+    }
+    *current_tick = tick;
+}
